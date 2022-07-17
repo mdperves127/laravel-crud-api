c5f81ba1448c1cddaafaed8a84ec36b62ea5c259
perveshossen <shikderm211@gmail.com>
2022-07-17 13:09:55 +0600

    Repository, Interface, Trait & Data Communication

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Respositories\ProductRepository;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    use ResponseTrait;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index():JsonResponse
+    {
+        try{
+
+        return $this->responseSuccess($this->productRepository->getAll(), "Product fetched successfully.");
+
+        } catch(Exception $e) {
+
+            return $this->responseError([], $e->getMessage());
+
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Respositories/ProductRepository.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Respositories;
+
+use App\Interfaces\CrudInterface;
+use App\Models\Product;
+use Illuminate\Contracts\Pagination\Paginator;
+
+class ProductRepository implements CrudInterface {
+
+    public function getAll():Paginator {
+        return Product::paginate(10);
+    }
+
+    public function getById(int $id):Product {
+        return Product::find($id);
+    }
+}
